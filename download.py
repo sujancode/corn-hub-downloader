5831667e102cea53226cf557893b30c74983ad78
Sujan <sujancode@gmail.com>
2022-12-05 06:40:11 -0500

    Sup

--- download.py
+++ download.py
@@ -2,18 +2,20 @@ import youtube_dl
 import random
 import string
 
-res = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits, k=7))
-ydl_opts_start = {
-    'format': 'best',
-    'playliststart:': 1,
-    'playlistend': 4,
-    'outtmpl': f"./data/{res}.mp4",
-    'nooverwrites': True,
-    'no_warnings': False,
-    'ignoreerrors': True
-}
+while True:
+    res = ''.join(random.choices(string.ascii_uppercase +
+                                string.digits, k=7))
 
-url = "https://www.pornhub.com/pornstar/eva-elfie"
-with youtube_dl.YoutubeDL(ydl_opts_start) as ydl:
-    ydl.download([url])
+    ydl_opts_start = {
+        'format': 'best',
+        'playliststart:': 1,
+        'playlistend': 4,
+        'outtmpl': f"./data/{res}.mp4",
+        'nooverwrites': True,
+        'no_warnings': False,
+        'ignoreerrors': True
+    }
+
+    url = "https://www.pornhub.com/pornstar/eva-elfie"
+    with youtube_dl.YoutubeDL(ydl_opts_start) as ydl:
+        ydl.download([url])
